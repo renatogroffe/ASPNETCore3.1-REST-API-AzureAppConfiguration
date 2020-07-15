e41ac9c88f4359761eb080ebb2e7151f5d362c95
Renato Groffe <renato.groffe@yahoo.com.br>
2020-07-15 00:50:26 -0300

    ASP.NET Core 3.1 + REST API + Contagem de Acessos + Azure App Configuration
    ASP.NET Core 3.1 + REST API + Contagem de Acessos + Azure App Configuration

---
+++ APIContagem/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace APIContagem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    var settings = config.Build();
+                    config.AddAzureAppConfiguration(options =>
+                    {
+                        options.Connect(settings["ConnectionStrings:AppConfiguration"])
+                            .ConfigureRefresh(refresh =>
+                            {
+                                refresh.Register("TesteAmbiente");
+                            });
+                    });
+                })     
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
